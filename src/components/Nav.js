f8569897373614ee7428bf6c83e3ad41343c0024
David <david.buckley@meritacademy.org>
2020-09-28 15:27:34 +0000

    react router dom demo

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <nav>
+    <Link to="/">Page1</Link>
+    <Link to="/2">Page2</Link>
+  </nav>
+)
+
+export default Nav
---
+++ src/pages/HomePages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const Page1 = () => (
+  <>
+    <Nav />
+    <h1>Page1</h1>
+  </>
+)
+
+const Page2 = () => (
+  <>
+    <Nav />
+    <h1>Page2</h1>
+  </>
+)
+
+export { Page1, Page2 };
